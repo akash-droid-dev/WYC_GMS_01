965f08f719c6b7a6adc873391d6bac7e0464df6d
akash-droid-dev <akash.rathore7704@gmail.com>
2026-02-18 15:29:39 +0530

    chore: wipe repository

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // GitHub Pages: /WYC_GMS_01/ for https://akash-droid-dev.github.io/WYC_GMS_01/
-  base: '/WYC_GMS_01/',
-})
